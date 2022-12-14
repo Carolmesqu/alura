880c80fd2459fe73998331a75831124ec66dc9ee
carolmesqu <carolmesqu@gmail.com>
2022-12-14 19:42:01 -0300

    exemplos de aulas

---
+++ Fundamentos_JS/type-boolean.js
@@ -0,0 +1,15 @@
+/*boolean 
+   true > verdadeiro
+   false > falso 
+*/
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastradoAtivado = true;
+
+console.log(primeiroNumero === segundoNumero);
+
+const text1 = "Alura";
+const text2 = "alura";
+
+const textoMinusculo = text1.toLowerCase();
+console.log(textoMinusculo === text2);
