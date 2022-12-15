f0d6f4903a86647dc321d4bc0edbd2827db75492
carolmesqu <carolmesqu@gmail.com>
2022-12-14 21:46:47 -0300

    algumas informacoes interessantes.

--- Fundamentos_JS/conversoes.js
+++ Fundamentos_JS/conversoes.js
@@ -37,3 +37,25 @@ let usuarioConectadoO = false;
 console.log(Number(usuarioConectadoO)); // teremos a conversão da booleana para número, sendo que false (falso) retorna o número 0.
 usuarioConectado = true;
 console.log(Number(usuarioConectado)); // agora teremos a conversão de true (verdadeiro) para o número 1.
+
+var respostaDeTudo = 42
+let idade = 29
+const pi = 3.14
+
+{
+    var respostaDeTudo = 3.14
+    let idade = 42
+    const pi = 29
+    console.log(respostaDeTudo, idade, pi)
+
+}
+console.log(respostaDeTudo, idade, pi)
+
+/**
+ * Variáveis declaradas com var ou let podem ser reatribuídas. 
+ * Porém, uma variável declarada com let só pode ser usada no 
+ * mesmo escopo em que está definida. No caso da declaração const, 
+ * significa que a variável sempre terá uma referência ao mesmo objeto 
+ * ou valor primitivo, porque essa referência não pode ser alterada. 
+ * A referência em si é imutável, mas o valor mantido pela variável não se torna imutável.
+ */
