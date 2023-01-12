08b0afd6d58bd585000661155ccfd74ef9bc6fb9
carolmesqu <carolmesqu@gmail.com>
2023-01-12 07:27:43 -0300

    JSON.parse, JSON.stringify, includes, hasOwnProperty e sort

--- Objetos_JS/leitura.js
+++ Objetos_JS/leitura.js
@@ -7,3 +7,16 @@ const dados = require("./cliente.json");
 
 console.log(dados);
 console.log(typeof dados);
+
+const clienteEmString = JSON.stringify(dados);
+
+console.log(clienteEmString);
+console.log(typeof clienteEmString);
+
+console.log(clienteEmString.nome);
+
+JSON.parse(clienteEmString);
+
+const objetoCliente = JSON.parse(clienteEmString);
+
+console.log(objetoCliente.nome);
